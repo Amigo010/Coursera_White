81a9df050d63a22459532ab8e0ba59ae0311c265
StasV <donsgenii@gmail.com>
2019-10-17 19:49:50 +0300

    Solved w4_finct..

--- w4_functia_ensure/main.cpp
+++ w4_functia_ensure/main.cpp
@@ -1,4 +1,23 @@
 //
 // Created by stas on 17.10.19.
 //
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+void EnsureEqual(const string& left, const string& right){
+    if(left != right){
+        throw runtime_error(left + " != " + right);
+    }
+}
+
+int main() {
+    try {
+        EnsureEqual("C++ White", "C++ White");
+        EnsureEqual("C++ White", "C++ Yellow");
+    } catch (runtime_error& e) {
+        cout << e.what() << endl;
+    }
+    return 0;
+}
