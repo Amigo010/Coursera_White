f3f86d8504f329a46f4b335157416ea57d4ed126
StasV <donsgenii@gmail.com>
2019-10-18 17:31:01 +0300

    w4_rabit has been solved

--- w4_rabota_so_vrem/main.cpp
+++ w4_rabota_so_vrem/main.cpp
@@ -1,4 +1,23 @@
 //
 // Created by stas on 18.10.19.
 //
+#include <string>
+#include <system_error>
 
+using namespace std;
+
+string AskTimeServer();
+
+class TimeServer{
+public:
+    string GetCurrentTime(){
+        try{
+            LastFetchedTime = AskTimeServer();
+        }
+        catch (system_error& system_error){}
+
+        return LastFetchedTime;
+    };
+private:
+    string LastFetchedTime = "00:00:00";
+};
